0abde5419a09ab0ca700343fade8080d25ca4968
Jon Luengo <jonluengo@MacBook-Air-de-Jon.local>
2024-04-29 13:42:03 +0200

    less duplicity in cache

--- RickAndMorty/RickAndMorty/Utilities/ImageLoadingAndCaching.swift
+++ RickAndMorty/RickAndMorty/Utilities/ImageLoadingAndCaching.swift
@@ -9,9 +9,10 @@ import Foundation
 import UIKit
 
 final class ImageLoadingAndCaching {
+    private let networkManager = NetworkManager()
     
     func getImage(from url: URL, completion: @escaping (UIImage?) -> Void) {
-        if let imageDataFromCache = fetchFromCache(url: url.absoluteString) {
+        if let imageDataFromCache = networkManager.fetchFromCache(url: url.absoluteString) {
             if let imageFromCache = UIImage(data: imageDataFromCache) {
                 completion(imageFromCache)
             }
@@ -26,7 +27,7 @@ final class ImageLoadingAndCaching {
         getData(from: url) { imageData, response, error in
             guard let imageData = imageData, error == nil else { return }
             if let image = UIImage(data: imageData) {
-                self.saveInCache(url: url.absoluteString, data: imageData)
+                self.networkManager.saveInCache(url: url.absoluteString, data: imageData)
                 completion(image)
             }
         }
@@ -34,21 +35,6 @@ final class ImageLoadingAndCaching {
     
     private func getData(from url: URL, completion: @escaping (Data?, URLResponse?, Error?) -> ()) {
         URLSession.shared.dataTask(with: url, completionHandler: completion).resume()
-    }
-    
-    func saveInCache(url: String, data: Data) {
-        let cache = RealmManager()
-        let cacheObject = RealmCacheObject(serviceData: data, url: url)
-        let _ = cache.saveObject(cacheObject: cacheObject)
-    }
-    
-    func fetchFromCache(url: String) -> Data? {
-        let cache = RealmManager()
-        if let fetchedData = cache.fetchObjects(url: url) {
-            return fetchedData
-        } else {
-            return nil
-        }
     }
     
 }
